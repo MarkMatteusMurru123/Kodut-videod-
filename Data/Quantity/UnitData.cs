c3fbc4d900fdbf80db478e7b0767bbc857572460
Mark Matteus Murru <mamurr@ttu.ee>
2020-03-01 23:16:37 +0200

    8min proge

---
+++ Data/Quantity/UnitData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Data.Common;
+
+namespace Abc.Data.Quantity
+{
+    class UnitData : DefinedEntityData
+    {
+        public string MeasureId { get; set; }
+    }
+}
