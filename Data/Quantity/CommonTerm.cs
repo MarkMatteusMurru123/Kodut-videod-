87664c0178defe6aa76e3045e72b211df618579b
Mark Matteus Murru <markmatteus12@gmail.com>
2020-03-28 19:28:14 +0200

    45. video tehtud, 44 jäi vahele

---
+++ Data/Quantity/CommonTerm.cs
@@ -0,0 +1,10 @@
+﻿using Abc.Data.Common;
+namespace Abc.Data.Quantity
+{
+    public class CommonTerm : PeriodData
+    {
+        public string MasterId { get; set;  }
+        public string TermId { get; set;}
+        public int Power{ get; set;  }
+    }
+}
